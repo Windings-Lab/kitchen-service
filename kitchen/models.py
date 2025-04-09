66affc94b7318f43fbb5a890f3147c80bf00bb5b
Windings-Lab <janton20001@gmail.com>
2025-04-09 18:33:17 +0300

    Create imageresize mixin to have 400x400 constraints

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,12 +1,27 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from PIL import Image
+
+
+class ImageResizeMixin:
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)  # Save original image
+
+        img_path = self.image.path
+        img = Image.open(img_path)
+
+        # Resize logic
+        max_size = (400, 400)  # Thumbnail size
+        img.thumbnail(max_size)
+
+        img.save(img_path)  # Overwrite the original
 
 
 class Cook(AbstractUser):
     years_of_experience = models.IntegerField(default=0)
 
 
-class DishType(models.Model):
+class DishType(ImageResizeMixin, models.Model):
     name = models.CharField(max_length=255, unique=True)
     image = models.ImageField(
         upload_to="dish_type/",
@@ -18,7 +33,7 @@ class DishType(models.Model):
         return self.name
 
 
-class Ingredient(models.Model):
+class Ingredient(ImageResizeMixin, models.Model):
     name = models.CharField(max_length=255, unique=True)
     image = models.ImageField(
         upload_to="ingredient/",
@@ -30,7 +45,7 @@ class Ingredient(models.Model):
         return self.name
 
 
-class Dish(models.Model):
+class Dish(ImageResizeMixin, models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     price = models.IntegerField()
