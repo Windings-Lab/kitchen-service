fe798bebccbaf49d3e7010ea6b5cb26a4085ee27
Windings-Lab <janton20001@gmail.com>
2025-04-17 14:24:25 +0300

    Add docstrings to utility functions for clarity
    Added detailed docstrings to `split_by_capitals` and `create_route` functions to explain their purpose, input parameters, and return values.

--- utility/__init__.py
+++ utility/__init__.py
@@ -5,11 +5,33 @@ from django.db.models import QuerySet
 
 
 def split_by_capitals(text):
-    # Regular expression to match capitalized words
+    """
+    Splits a given string into a list of words based on capitalized letters. Each word
+    starts with an uppercase letter followed by any number of lowercase letters.
+
+    :param text: Input string to be split.
+    :type text: str
+    :return: A list of words derived from the input string, where each word starts
+        with an uppercase letter followed by lowercase letters.
+    :rtype: list[str]
+    """
     return re.findall(r'[A-Z][a-z]*', text)
 
 
 def create_route(class_name: str):
+    """
+    Generate a route string by splitting a given class name into words.
+
+    Creates a URL-friendly, lowercase string by splitting the input class
+    name into separate words wherever a capital letter is encountered, then
+    joining these words with hyphens.
+
+    :param class_name: The input class name to transform into a route string.
+    :type class_name: str
+    :return: A URL-friendly, hyphenated, lowercase string derived from the
+        given class name.
+    :rtype: str
+    """
     return "-".join([
             item.lower()
             for item in split_by_capitals(class_name)
